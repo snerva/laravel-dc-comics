e6275b9005b35df1b9e19dc08b60e2d62683af6c
snerva <selenenerva@live.it>
2022-12-22 19:13:42 +0100

    add delete

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -45,6 +45,31 @@
                             <a href="{{route('comics.show',$comic->id)}}" class="btn btn-primary view"><i class="fa-regular fa-eye px-1"></i></a>
                             <a href="{{route('comics.edit', $comic->id)}}" class="btn btn-secondary edit"><i class="fa-regular fa-pen-to-square px-1"></i></a>
                             <a href="" class="btn btn-danger delete"><i class="fa-solid fa-trash px-1"></i></a>
+
+                            <div class="modal fade" id="deleteProduct-{{$comic->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId-{{$comic->id}}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="modalTitleId-{{$comic->id}}">Delete Product</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Are you sure to destroy it?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <form action="{{route('comics.destroy', $comic->id)}}" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">
+                                                    Confirm
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </td>
                     </tr>
                     @empty
